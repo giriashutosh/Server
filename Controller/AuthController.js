6c544e57c17303708bc641b07217afdcc3f7d8ea
ramankumar12995 <ramankumar12995@gmail.com>
2023-04-29 14:58:11 +0530

    do some changes in AuthController.js

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,26 +1,32 @@
 import UserModel from "../Modals/userModel.js";
 import bcrypt from "bcrypt"
-
+import jwt from "jsonwebtoken"
 //Registering a new user
 export const registerUser = async (req, res) => {
-    const { username, password, firstname, lastname } = req.body;
-    
-    const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(password, salt);
+  //const { username, password, firstname, lastname } = req.body;
 
-    const newUser = new UserModel({
-        username,
-        password: hashedPass,
-        firstname,
-        lastname,
-    });
-    
-    try {
-        await newUser.save();
-        res.status(200).json(newUser);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+  req.body.password = hashedPass
+  const newUser = new UserModel(req.body);
+  const { username } = req.body;
+  try {
+    //addition new
+    const oldUser = await UserModel.findOne(username)
+    if (oldUser) {
+      return res.status(400).json({ message: "user already exists" })
+    }
+    //changed
+    const user = await newUser.save();
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.JWT_KEY,
+      {expiresIn: "1h"}
+    )
+    res.status(200).json({user, token});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 //LOGIN USER
@@ -31,12 +37,20 @@ export const loginUser = async (req, res) => {
     const user = await UserModel.findOne({ username: username })
     if (user) {
       const validity = await bcrypt.compare(password, user.password)
-      validity ? res.status(200).json(user) : res.status(400).json("wrong password")
+      if (!validity) {
+        res.status(400).json("wrong password")
+      } else {
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWT_KEY,
+          {expiresIn: "1h"}
+        )
+      }
     } else {
-      res.status(404).json('User does not exist')
+      res.status(404).json('User not found')
     }
   }
   catch (error) {
-    console.log(error)
+    res.status(500).json(error)
   }
 }
