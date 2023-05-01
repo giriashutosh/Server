37ecd4988a0c01d4b6858e33268bebeedaee33f9
ramankumar12995 <ramankumar12995@gmail.com>
2023-05-01 16:30:49 +0530

    added some new route

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ import cors from "cors";
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
-
+import UploadRoute from './Routes/UploadRoute.js'
 const app = express()
+
+
+// to serve images for public 
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
 
 //Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
@@ -28,4 +33,5 @@ mongoose
 // usage of routes
 app.use('/auth', AuthRoute)
 app.use('/user', UserRoute)
-app.use('/post',PostRoute)
+app.use('/post', PostRoute)
+app.use('/upload', UploadRoute)
